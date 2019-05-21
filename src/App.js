31aa77f303dfe71ce6581fd9238457f2782c8874
Sam Parsons <samparsons269@gmail.com>
2019-05-20 22:11:50 -0400

    let there be crude sounds

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ function toggleBox(priorChecked, i, row) {
 export default class App extends React.PureComponent {
   state = {
     checked: [
-      [false, false, false, false, false, false, false, false],
-      [false, false, false, false, false, false, false, false]
+      [true, true, false, false, false, false, false, false],
+      [false, false, true, false, true, false, true, false]
     ],
     isPlaying: false,
     sequenceLength: 8,
-    tempo: 120,
+    tempo: 180,
+    renderedNotes: [],
+    partContainer: [],
+    notes: ["C5", "Eb5"],
     elapsedTime: 0,
     lastTime: 0,
     numberOfTaps: 0,
@@ -34,7 +37,68 @@ export default class App extends React.PureComponent {
   };
 
   componentDidMount = () => {
+    const synth = new Tone.PolySynth(2, Tone.Synth).toMaster();
     Tone.Transport.start();
+
+    // generate metronome
+    // erase or stop all previous parts
+    const partContainer = this.state.partContainer;
+    partContainer.forEach(part => part.removeAll());
+
+    // metronome vitals
+    const [note1, note2] = this.state.notes;
+    const seqLength = this.state.sequenceLength;
+    const matrix = this.state.checked;
+    const tempo = this.state.tempo;
+
+    // new renderedNotes array, populate
+    const renderedNotes = [];
+    for (let i = 0; i < seqLength; i++) {
+      if (matrix[0][i]) {
+        renderedNotes.push({
+          note: note2,
+          time: `0:${(i * 60) / tempo}`,
+          velocity: 0.05
+        });
+      } else if (!matrix[1][i]) {
+        renderedNotes.push({
+          note: note2,
+          time: `0:${(i * 60) / tempo}`,
+          velocity: 0
+        });
+      }
+      if (matrix[1][i]) {
+        renderedNotes.push({
+          note: note1,
+          time: `0:${(i * 60) / tempo}`,
+          velocity: 0.05
+        });
+      }
+    }
+
+    // create new Part, start Part, push Part to container
+    const part = new Tone.Part((time, value) => {
+      synth.triggerAttackRelease(value.note, 0.05, time, value.velocity);
+      console.log("sound");
+    }, renderedNotes).start(0);
+    partContainer.push(part);
+
+    this.setState(
+      {
+        renderedNotes,
+        partContainer
+      },
+      () => {
+        // configure looping for step sequencer
+        Tone.Transport.loop = true;
+        Tone.Transport.loopStart = 0;
+        Tone.Transport.loopEnd = (seqLength * 30) / this.state.tempo; // magic equation
+
+        // start transport after loop config
+        Tone.Transport.start("+0.0");
+        console.log("playing initiated");
+      }
+    );
   };
 
   onToggleBox = (i, row) => {
